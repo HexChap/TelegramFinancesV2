9dba2251f0d80754eb23b3d8463031e5872d4266
Kirill Ibragimov <hexchap@gmail.com>
2023-01-15 14:16:37 +0200

    hotfix

--- TFinances/handlers/user.py
+++ TFinances/handlers/user.py
@@ -8,10 +8,15 @@ from aiogram import types, filters
 @dispatcher.message_handler(filters.Text("👨‍ Профиль"))
 async def me(msg: types.Message, is_inner: bool = False):
     t_id = msg.from_user.id
-    user_not_found = f"Creating user with t_id={t_id} because was not found."
 
-    if is_inner:
-        logger.debug(user_not_found)
+    if is_inner:  # Saves one get query
+        await UserCRUD.create_by(
+            UserSchema(
+                telegram_id=t_id,
+                lang=msg.from_user.language_code
+            )
+        )
+        logger.debug(f"Creating user with t_id={t_id} because was not found.")
 
     if not (user := await UserCRUD.get_by_telegram_id(t_id)):
         user = await UserCRUD.create_by(
@@ -20,7 +25,7 @@ async def me(msg: types.Message, is_inner: bool = False):
                 lang=msg.from_user.language_code
             )
         )
-        logger.debug(user_not_found)
+        logger.debug(f"Creating user with t_id={t_id} because was not found.")
 
     await msg.reply(
         f"💼 Hello, {msg.from_user.first_name}!\n\n"
